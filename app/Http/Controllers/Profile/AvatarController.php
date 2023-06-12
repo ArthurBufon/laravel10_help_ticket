e9f5ab18ca319a4ca5cc78b14784ab85758aa2e0
arthur_infus <arthur@infus.inf.br>
2023-06-12 17:33:22 -0300

    armazenando avatar no disco publico

--- app/Http/Controllers/Profile/AvatarController.php
+++ app/Http/Controllers/Profile/AvatarController.php
@@ -4,13 +4,26 @@ namespace App\Http\Controllers\Profile;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\UpdateAvatarRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class AvatarController extends Controller
 {
     public function update(UpdateAvatarRequest $request)
     {
-        dd($request->all());
+        $id = auth()->user()->id;
+        $user = User::find($id);
+
+        $path = Storage::disk('public')->put('avatars', $request->file('avatar'));
+        // $path = $request->file('avatar')->store('avatars', 'public');
+
+        if ($old_avatar = $user->avatar)
+        {
+            Storage::disk('public')->delete($old_avatar);
+        }
+
+        $user->update(['avatar' => $path]);
 
         return back()->with('message', 'Avatar updated.');
     }
